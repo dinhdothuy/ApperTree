a44ad47b4d1ace5510d504cd5be6919eb78275b8
Daisy Dinh <daisydodinh@gmail.com>
2019-01-11 14:34:13 -0600

    post version 1

---
+++ client/build/precache-manifest.ce3e7e886a317df633723046415b1ca5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cd22bacb0fb92c2e09db",
+    "url": "/static/css/main.77392d91.chunk.css"
+  },
+  {
+    "revision": "cd22bacb0fb92c2e09db",
+    "url": "/static/js/main.cd22bacb.chunk.js"
+  },
+  {
+    "revision": "7425b6c15647fe9ab97c",
+    "url": "/static/js/1.7425b6c1.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "efbaca6b261512a0844e68cf9d79af11",
+    "url": "/index.html"
+  }
+];
